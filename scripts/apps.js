0fae9e875a0e78e750fef563e167541beb63cb80
vivartmedia <nanno70@yahoo.de>
2023-11-27 20:37:50 -0800

    updated html and js

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,14 +1,8 @@
 import {apikey} from "./environment.js"
 
-// function ApiCall() {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${ApiKey}`)
-//         .then((response) => {
-//             return response.json()
-//         })
-//         .then((data) => {
-//             console.log(data);
-//     })
-// }
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('kdate');
+const currentWeatherItemsEl = document.getElementById('current-weather-item');
 
 function apiCall() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.694248118402&lon=-120.98107010612894&appid=${apikey}`)
